c2d3952a984ac461e211ae271b09dd1eed42fea9
wangnan0623 <152370051+wangnan0623@users.noreply.github.com>
2026-03-13 19:05:41 +0800

    Add files via upload

---
+++ pytracking/run.sh
@@ -0,0 +1,9 @@
+python run_tracker.py dimp prdimp18 --dataset eotb --sequence val
+
+python run_tracker.py kys default --dataset eotb --sequence val
+
+python run_tracker.py dimp afnet --dataset eotb --sequence val --epochname DiMPnet_ep0052.pth.tar
+
+/home/wn/project/AFNet-main/logs/FE240/checkpoints/ltr/afnet/afnet
+
+DiMPnet_ep0052.pth.tar
